37cd21facf96622787e2ab747448bdfc17eaef74
aadivyaraushan <96186374+aadivyaraushan@users.noreply.github.com>
2022-08-22 23:43:19 +0400

    chore(deployment): fixes navbar issue

--- client/src/components/common/Topbar.jsx
+++ client/src/components/common/Topbar.jsx
@@ -19,19 +19,14 @@ const Topbar = () => {
 
   return (
     <div className='flex flex-row h-16 justify-between'>
-      <div className='bg-neutral-50 flex items-center shadow-lg w-28 h-10 rounded-xl mt-4 ml-4'>
-        <p className='text-xl mx-auto'>Supply Chain Assistant</p>
+      <div className='bg-neutral-50 flex items-center shadow-lg w-42 h-10 rounded-xl mt-4 ml-4 py-4 px-8'>
+        <p>1MATIC = ${currencyConversion}</p>
       </div>
-      <div className='flex flex-row justify-between'>
-        <div className='bg-neutral-50 flex items-center shadow-lg w-42 h-10 rounded-xl mt-4 ml-4 py-4 px-8'>
-          <p>1MATIC = ${currencyConversion}</p>
-        </div>
-        <Button className='bg-neutral-50 flex justify-self-end items-center justify-center shadow-lg w-28 h-10 rounded-xl mt-4 ml-4 mr-4 '>
-          <Link to='/'>
-            <p className='text-xl'>Log Out</p>
-          </Link>
-        </Button>
-      </div>
+      <Button className='bg-neutral-50 flex justify-self-end items-center justify-center shadow-lg w-28 h-10 rounded-xl mt-4 ml-4 mr-4 '>
+        <Link to='/'>
+          <p className='text-xl'>Log Out</p>
+        </Link>
+      </Button>
     </div>
   );
 };
